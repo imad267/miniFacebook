6c00ad6c590c6f944cbbca0d3ec3e448773230d6
Deji Adeyemo <charleeadeyemo@hotmail.com>
2020-03-04 13:13:20 +0100

    I added SignUp and Login Page(Just Views_No Code)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,38 @@
 @extends('templates.default')
 
 @section('content')
-    <h3>Welcome to miniFacebook</h3>
-    <p>The best social network, like.... ever.</p>
+    <div class="row">
+        <div class="col-6">
+            <h3>Sign Up</h3>
+            <form action="#" method="post">
+                <div class="form-group">
+                    <label for="email">Your E-mail</label>
+                    <input class="form-control" type="text" name="email" id="email">
+                </div>
+                <div class="form-group">
+                    <label for="first_name">Your First Name</label>
+                    <input class="form-control" type="text" name="first_name" id="first_name">
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input class="form-control" type="password" name="password" id="password">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+        <div class="col-6">
+            <h3>Sign In</h3>
+            <form action="#" method="post">
+                <div class="form-group">
+                    <label for="email">Your E-mail</label>
+                    <input class="form-control" type="text" name="email" id="email">
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input class="form-control" type="password" name="password" id="password">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    </div>
 @stop
